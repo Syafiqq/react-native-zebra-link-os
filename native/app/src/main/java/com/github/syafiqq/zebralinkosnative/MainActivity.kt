50ec16139a564b85e27492ca366a54bd57984fcb
msyafiqq <muhammad.syafiq@geniebook.com>
2025-05-07 20:00:59 +0700

    test(discovery) add main activity test

--- native/app/src/main/java/com/github/syafiqq/zebralinkosnative/MainActivity.kt
+++ native/app/src/main/java/com/github/syafiqq/zebralinkosnative/MainActivity.kt
@@ -1,20 +1,127 @@
 package com.github.syafiqq.zebralinkosnative
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+    }
+
+    fun onRequestDiscoverTapped(view: View) {
+        onRequestDiscover()
+    }
+
+    fun onRequestDiscover() {
+        if (checkBluetoothPermission()) {
+            if (enableBluetooth()) {
+                startActivity(DiscoverPrinterActivity.newIntent(this@MainActivity))
+            }
+        }
+    }
+
+    private fun checkBluetoothPermission(): Boolean {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.BLUETOOTH_CONNECT
+                ) == PackageManager.PERMISSION_DENIED || ContextCompat.checkSelfPermission(
+                    this, Manifest.permission.BLUETOOTH_SCAN
+                ) == PackageManager.PERMISSION_DENIED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf<String>(
+                        Manifest.permission.BLUETOOTH_CONNECT, Manifest.permission.BLUETOOTH_SCAN
+                    ), 3000
+                )
+            } else {
+                if ((ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED) || (ContextCompat.checkSelfPermission(
+                        this, Manifest.permission.ACCESS_FINE_LOCATION
+                    ) != PackageManager.PERMISSION_GRANTED)
+                ) {
+                    ActivityCompat.requestPermissions(
+                        this, arrayOf<String>(
+                            Manifest.permission.ACCESS_COARSE_LOCATION,
+                            Manifest.permission.ACCESS_FINE_LOCATION
+                        ), 4000
+                    )
+                } else {
+                    return true
+                }
+            }
+        } else {
+            if ((ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED) || (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED)
+            ) {
+                ActivityCompat.requestPermissions(
+                    this, arrayOf<String>(
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                    ), 4000
+                )
+            } else {
+                return true
+            }
+        }
+        return false
+    }
+
+    private fun enableBluetooth(): Boolean {
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter != null) {
+            if (!bluetoothAdapter.isEnabled) {
+                Toast.makeText(this, "Please switch on the bluetooth", Toast.LENGTH_SHORT).show()
+            } else {
+                return true
+            }
+        } else {
+            // Device does not support Bluetooth
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show()
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int, permissions: Array<String?>, grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 3000) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onRequestDiscover()
+            } else {
+                // Location permission denied, show a message or handle accordingly
+                Toast.makeText(
+                    this, "Location permission required for Bluetooth scanning", Toast.LENGTH_SHORT
+                ).show()
+            }
+        } else if (requestCode == 4000) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                onRequestDiscover()
+            } else {
+                // Location permission denied, show a message or handle accordingly
+                Toast.makeText(
+                    this, "Location permission required for Bluetooth scanning", Toast.LENGTH_SHORT
+                ).show()
+            }
         }
     }
 }
